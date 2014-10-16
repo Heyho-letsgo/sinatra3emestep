c4813c43e46736adbd081f55b07b610f7b0f59b2
André Baudin-Laurencin <abaudin@gmail.com>
2014-10-16 15:43:50 +0200

    modif du mdp de postgress

--- config/environments.rb
+++ config/environments.rb
@@ -1,24 +1,6 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
-# configure :production, :development do
-#   #db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-#   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/blog.db')
-#
-#
-#
-#
-#   ActiveRecord::Base.establish_connection(
-#       :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#       :host     => db.host,
-#       :username => db.user,
-#       :password => db.password,
-#       :database => db.path[1..-1],
-#       :encoding => 'utf8'
-#   )
-# end
-
-
-configure :production, :development, :test do
+configure :production, :development do
   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/blog')
 
   ActiveRecord::Base.establish_connection(
